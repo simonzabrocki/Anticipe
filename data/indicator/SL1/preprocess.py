7e44a5a1c64977758f28e46bece2e43207ff2318
Simon <simon.zabrocki@gmail.com>
2021-07-22 20:48:59 +0900

    update SL1

--- data/indicator/SL1/preprocess.py
+++ data/indicator/SL1/preprocess.py
@@ -9,29 +9,34 @@ import pandas as pd
 #     return df
 
 
-# def process_SL1():
-#     df = pd.read_csv('data/indicator/SL1/raw/SL1_FAO.M.csv')
-#     df = df.drop(columns=['Domain', 'Domain Code', 'Area Code', 'Element Code',
-#                           'Item Code', 'Year Code', 'Flag', 'Flag Description', 'Unit', 'Element'])
-#     pivoted = df.pivot(index=['Area', 'Year'], columns='Item', values='Value')
-#
-#     NB = pivoted['Synthetic Fertilizers'] \
-#         + pivoted['Manure applied to Soils'] \
-#         + pivoted['Atmospheric Deposition'] \
-#         + pivoted['Biological Fixation'] \
-#         - pivoted['Crop Removal']
-#
-#     NB = NB.reset_index()
-#     NB.columns = ['Country', 'Year', 'Value']
-#     NB['Value'] = abs(NB['Value'])
-#
-#     return NB
-
 
 def process_SL1():
+    df = pd.read_csv('data/indicator/SL1/raw/SL1_FAO.M.csv').query("Element == 'Cropland nutrient flow per unit area'")
+    df = df.drop(columns=['Domain', 'Domain Code', 'Area Code (FAO)', 'Element Code',
+                          'Item Code', 'Year Code', 'Flag', 'Flag Description', 'Unit', 'Element'])
+    
+    pivoted = df.pivot(index=['Area', 'Year'], columns='Item', values='Value')
 
-    df = pd.read_csv('data/indicator/SL1/raw/SL1_FAO.M.csv')
-    df = df.rename(columns={'Area': 'Country'})
-    # df['Value'] = abs(df['Value'])
-    df[df['Value'] < 0] = 5 - df[df['Value'] < 0]  # mirror
-    return df[['Country', 'Year', 'Value']]
+    NB = pivoted['Synthetic Fertilizers'] \
+        + pivoted['Manure applied to Soils'] \
+        + pivoted['Atmospheric Deposition'] \
+        + pivoted['Biological Fixation'] \
+        - pivoted['Crop Removal']
+
+    NB = NB.reset_index()
+    NB.columns = ['Country', 'Year', 'Value']
+    NB['Value'] = abs(NB['Value'])
+
+    
+    NB[NB['Value'] < 0] = 5 - NB[NB['Value'] < 0]  # mirror
+    return NB
+
+
+
+# def process_SL1():
+
+#     df = pd.read_csv('data/indicator/SL1/raw/SL1_FAO.M.csv')
+#     df = df.rename(columns={'Area': 'Country'})
+#     # df['Value'] = abs(df['Value'])
+#     df[df['Value'] < 0] = 5 - df[df['Value'] < 0]  # mirror
+#     return df[['Country', 'Year', 'Value']]
