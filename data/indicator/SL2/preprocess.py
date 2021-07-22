6c6c2134780cf78937a23614649fb37aaa6df2c2
Simon <simon.zabrocki@gmail.com>
2021-07-22 17:16:57 +0900

    Update SL2

--- data/indicator/SL2/preprocess.py
+++ data/indicator/SL2/preprocess.py
@@ -1,7 +1,20 @@
 import pandas as pd
 
 
-def process_SL2():
+# def process_SL2():
 
-    df = pd.read_csv('data/indicator/SL2/raw/SL2_GGGI.M.csv')
-    return df[['ISO', 'Year', 'Value']]
+#     df = pd.read_csv('data/indicator/SL2/raw/SL2_GGGI.M.csv')
+#     return df[['ISO', 'Year', 'Value']]
+
+def process_SL2():
+    df = (
+        pd.read_csv('data/indicator/SL2/raw/SL2_FAO.M.csv')[['Year', 'Area', 'Item', 'Value']]
+          
+    )
+    agri_orga = df.query('Item == "Agriculture area under organic agric."').set_index(["Year", 'Area'])[['Value']]
+    agri_tot = df.query('Item == "Agricultural land"').set_index(['Year', 'Area'])[['Value']]
+    
+    df = (agri_orga / agri_tot * 100).reset_index().dropna().rename(columns={'Area': 'Country'})
+    
+    
+    return df
