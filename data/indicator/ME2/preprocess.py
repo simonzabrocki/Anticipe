5d1ea45395a0746785565b1e4bd4d082a183432a
Simon <simon.zabrocki@gmail.com>
2021-07-22 16:55:43 +0900

    update ME2

--- data/indicator/ME2/preprocess.py
+++ data/indicator/ME2/preprocess.py
@@ -2,5 +2,10 @@ import pandas as pd
 
 
 def process_ME2():
-    df = pd.read_csv('data/indicator/ME2/raw/ME2_GGGI.M.csv', index_col=0)
+    df = (
+        pd.read_csv('data/indicator/ME2/raw/ME2_UNEP.M.csv')
+          .melt(id_vars=['Country', 'Flow Type'], var_name='Year', value_name='Value')
+          .rename(columns={"Flow Type": 'Description'}) 
+          .dropna()
+    )
     return df
