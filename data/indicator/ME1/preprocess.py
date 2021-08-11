32c65d7272dc788ea126dc9d96dc0c007decd753
Innocent50 <brathan42@gmail.com>
2021-08-11 17:53:34 +0200

    update ME1 indicator

--- data/indicator/ME1/preprocess.py
+++ data/indicator/ME1/preprocess.py
@@ -2,5 +2,8 @@ import pandas as pd
 
 
 def process_ME1():
-    df = pd.read_csv("data/indicator/ME1/raw/ME1_GGGI.M.csv")
+    df = pd.read_csv("data/indicator/ME1/raw/ME1_OECD.M.csv")
+    df = df.drop(columns=['INDICATOR', 'SUBJECT', 'MEASURE', 'FREQUENCY','Flag Codes'])
+    df.rename(columns={'LOCATION':'ISO', 'TIME':'Year'}, inplace=True)
+    
     return df
