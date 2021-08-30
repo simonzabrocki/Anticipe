ef8cf58b7bbb035c08e88783966f66c1fd7d1481
Innocent50 <brathan42@gmail.com>
2021-08-30 23:02:17 +0200

    Testing of result file

--- Scripts.py
+++ Scripts.py
@@ -4,39 +4,46 @@ import os
 
 
 # Test that results.csv is not empty
-# Test that there is not duplicates
-# Test that there is not na
-# Test that there is all indicator include
-# ...........
+def test_file_content():
+    path = f'data/full_data/result.csv'
+    df = pd.read_csv(path)
+    assert df.shape[0] > 0, f"No data in {path} "
 
-# Test that there is not nan
-def test_nan():
-    df = pd.read_csv('data/full_data/result.csv')
-    df.isnull().values.any()
-    
-# Test that results.csv is not empty
-def test_empty():
-    df = pd.read_csv('data/full_data/result.csv')
-    isempty = df.empty
-
-# Test that there is not duplicates
+    # Test that there is not duplicates
 def test_duplicate():
-    df = pd.read_csv('data/full_data/result.csv')
-    duplicate = df[df.duplicated()]
-
-
+    path = f'data/full_data/result.csv'
+    df = pd.read_csv(path)
+    duplicate = df.drop_duplicates(subset=['ISO', 'Variable', 'Year']).shape[0] - df.shape[0]
+    assert duplicate == 0, f"Duplicated values in {path}"
+    
+# Test that there is not NaN
+def test_missing_values():
+    path = f'data/full_data/result.csv'
+    df = pd.read_csv(path)
+    NaN = df.dropna(subset=['ISO', 'Year', 'Value', 'Variable','Aggregation']).shape[0] - df.shape[0]
+    assert NaN == 0, f"Missing values in {path}"
+    
 # Test that there is all indicator include
 Variables = [file for file in os.listdir('data/indicator/') if (len(file)==3 and file != 'TMP')]
 def test_Variables():
     df = pd.read_csv('data/full_data/result.csv')
     Variables_in_df = df.Variable.unique()
-   # assert set(Variables_in_df) == set(Variables)
-
+    assert set(Variables_in_df) == set(Variables)
 
 #Test columns
 def test_columns():
-    df = pd.read_csv('data/full_data/result.csv')
-    columns = df.columns
-    assert set(columns) >= set(['ISO', 'Year', 'Variable', 'Country', 'Aggregation', 'Value'])
+    path = f'data/full_data/result.csv'
+    df = pd.read_csv(path)
+    assert set(['ISO', 'Year', 'Variable','Aggregation', 'Value','Continent','UNregion','IncomeLevel']) <= set(df.columns), f"Columns are missing in {path}"
+
+
+#Test file presence 
+
+def test_file_presence():
+    path = f'data/full_data'
+    file = get_result_file_from_full_data(path)
+    assert len(files) > 0, f"No files in {path}"
+
+    
 
 
